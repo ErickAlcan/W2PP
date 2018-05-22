f0ff43d9858d08249548351eb73150d431b75e9f
VKlafke <VKlafke@outlook.com>
2018-05-22 19:53:26 -0300

    Update _MSG_UpdateItem.cpp

--- Code/TMSrv/_MSG_UpdateItem.cpp
+++ Code/TMSrv/_MSG_UpdateItem.cpp
@@ -1,5 +1,5 @@
 /*
-*   Copyright (C) {2015}  {Victor Klafke, Charles TheHouse}
+*   Copyright (C) {2015}  {VK, Charles TheHouse}
 *
 *   This program is free software: you can redistribute it and/or modify
 *   it under the terms of the GNU General Public License as published by
@@ -14,7 +14,6 @@
 *   You should have received a copy of the GNU General Public License
 *   along with this program.  If not, see [http://www.gnu.org/licenses/].
 *
-*   Contact at: victor.klafke@ecomp.ufsm.br
 */
 #include "ProcessClientMessage.h"
 
